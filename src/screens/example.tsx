e7cae29584409d92a52988913c5428f81b71092a
Cristian Rolando Reyes Chapoñan <creyeschaponan@gmail.com>
2024-06-10 18:53:31 -0500

    update proyecto base aynihogar

---
+++ src/screens/example.tsx
@@ -0,0 +1,34 @@
+<View style={styles.Group034}>
+        <Text style={styles.IntoduceTuNMeroDeTel}>
+          Intoduce tu número de Teléfono
+        </Text>
+        <Text style={styles.TeEnviaremosUnCDigoP}>
+          Te enviaremos un código para verificar tu número telefónico
+        </Text>
+        <View style={styles.BtnIntoducirNum}>
+          <Image
+            style={styles.Rectangle1}
+            source={{
+              uri: "https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/6ragp0se58w-178%3A2444?alt=media&token=26919779-a5d4-4b9d-83ea-e06d9e9aa6ce",
+            }}
+          />
+          <Text style={styles._51976380729}>+51 976380729</Text>
+          <Text style={styles.Pe}>PE</Text>
+          <Image
+            style={styles.Vector1}
+            source={{
+              uri: "https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/6ragp0se58w-178%3A2447?alt=media&token=d45f1da7-8b58-45fd-afac-3b04d7760a0f",
+            }}
+          />
+        </View>
+        <View style={styles.BotonEnviar}>
+          <Text style={styles.EnviarCDigo}>Enviar código</Text>
+        </View>
+        <View style={styles.BotonGoogle}>
+          <Text style={styles.Google}>Google</Text>
+          <Text style={styles.AlUnirteANuestraApli}>
+            Al unirte a nuestra aplicación aceptas nuestros Términos de Uso y
+            Pólitica de privacidad.
+          </Text>
+        </View>
+      </View>
